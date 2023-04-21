d20c0ec7b289f2a25f74ff9ef94f90fb0184ab08
Vasudev Patel <vasudev.patel@sap.com>
2023-04-21 13:17:47 -0700

    set theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { ShellBar, ThemeProvider } from "@ui5/webcomponents-react";
+import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme";
+import "@ui5/webcomponents-react/dist/Assets";
+
 import { CommunityList } from "./components/CommunityList";
 
 function App() {
+  // change theme to horizon
+  setTheme("sap_horizon");
+
   return (
     <ThemeProvider>
       <ShellBar primaryTitle="Irvine Company Apartments" />
