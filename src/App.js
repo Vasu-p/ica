8cae39d4e08271146b617869d1ccd05619786d5f
Vasudev Patel <vasudev.patel@sap.com>
2023-04-21 14:45:10 -0700

    add flexible column layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import { ShellBar, ThemeProvider } from "@ui5/webcomponents-react";
+import { useCallback, useState } from "react";
+
+import {
+  FCLLayout,
+  FlexibleColumnLayout,
+  ShellBar,
+  ThemeProvider,
+} from "@ui5/webcomponents-react";
+
 import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme";
 import "@ui5/webcomponents-react/dist/Assets";
 
 import { CommunityList } from "./components/CommunityList";
+import { ApartmentList } from "./components/ApartmentList";
 
 function App() {
   // change theme to horizon
   setTheme("sap_horizon");
+  const [layout, setLayout] = useState(FCLLayout.OneColumn);
+
+  const showApartmentDetails = useCallback(() => {}, []);
 
   return (
     <ThemeProvider>
       <ShellBar primaryTitle="Irvine Company Apartments" />
-      <CommunityList />
+      <FlexibleColumnLayout
+        startColumn={<CommunityList onShowDetails={showApartmentDetails} />}
+        midColumn={<ApartmentList communities={[]} />}
+        layout={layout}
+      />
     </ThemeProvider>
   );
 }
