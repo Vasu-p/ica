7b7feeeb5421db8254f88646b183059c226fc193
Vasudev Patel <vasudev.patel@sap.com>
2023-04-21 12:17:04 -0700

    add apis

---
+++ src/apis.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+
+export function getAllCommunities() {
+  return axios.post("https://search.irvinecompanyapartments.com/search", {
+    index: "prod_ica_community",
+    query: "",
+    params: {
+      hitsPerPage: 1000,
+      attributesToHighlight: [],
+      attributesToRetrieve: [
+        "osPropertyIds",
+        "cityName",
+        "calc_minBedrooms",
+        "calc_maxBedrooms",
+        "calc_minRent",
+        "calc_maxRent",
+        "communityName",
+      ],
+    },
+  });
+}
+
+export function getAllAvailableApartments(propertyIds) {
+  const propertyIdsFilter = propertyIds
+    .map((propertyId) => `(propertyID:${propertyId})`)
+    .join(" OR ");
+  console.log("propertyids filter", propertyIdsFilter);
+
+  return axios.post(
+    "https://search.irvinecompanyapartments.com/multipleQueries",
+    {
+      queries: [
+        {
+          indexName: "prod_ica_unitAvailability",
+          query: "",
+          params: {
+            hitsPerPage: 100,
+            attributesToHighlight: [],
+            filters: "propertyID:2265072",
+            attributesToRetrieve: [
+              "floorplanName",
+              "unitMarketingName",
+              "featuredAmenity",
+              "floorplanUniqueID",
+              "floorplanCRMID",
+              "unitCRMID",
+              "unitTypeCode",
+              "propertyID",
+              "propertyAddress",
+              "buildingNumber",
+              "floorplanID",
+              "floorplanBath",
+              "floorplanBed",
+              "floorplanDeposit",
+              "unitID",
+              "unitFloor",
+              "unitSqFt",
+              "unitIsStudio",
+              "unitTypeName",
+              "unitAmenities",
+              "unitStartingPrice",
+              "unitEarliestAvailable",
+              "unitConcessions",
+              "communityMarketingName",
+              "communityIDAEM",
+              "propertyZip",
+              "floorplanSqFt",
+            ],
+          },
+        },
+      ],
+    }
+  );
+}
