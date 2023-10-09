2b827b2408a4393747da5087fc44de35f02fc023
Vasudev Patel <vasudev.patel@sap.com>
2023-10-08 21:36:19 -0700

    add dummy filterbar

--- src/components/CommunityList.jsx
+++ src/components/CommunityList.jsx
@@ -4,14 +4,22 @@ import {
   Button,
   Toolbar,
   ToolbarSpacer,
+  IllustratedMessage,
+  FilterBar,
+  FilterGroupItem,
+  MultiComboBox,
+  MultiComboBoxItem,
+  Input,
+  Select,
 } from "@ui5/webcomponents-react";
+import "@ui5/webcomponents-fiori/dist/illustrations/BeforeSearch";
 
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { getAllCommunities } from "../apis";
 
 import { TableHeader } from "../common/TableHeader";
 
-export function CommunityList({ onShowDetails, ...otherprops }) {
+export function CommunityList({ onShowDetails, slot, ...otherprops }) {
   const [communityList, setCommunityList] = useState([]);
   const [selectedCommunities, setSelectedCommunities] = useState([]);
 
@@ -65,27 +73,65 @@ export function CommunityList({ onShowDetails, ...otherprops }) {
   );
 
   return (
-    <AnalyticalTable
-      header={
-        <Toolbar>
-          <ToolbarSpacer />
-          <Button onClick={handleShowDetails}>Show Details</Button>
-        </Toolbar>
-      }
-      data={communityList}
-      columns={columns}
-      groupBy={["city"]}
-      groupable={true}
-      filterable={true}
-      headerRowHeight={50}
-      selectionMode={AnalyticalTableSelectionMode.MultiSelect}
-      onRowSelect={(e) => {
-        console.log("selected rowws", e.detail.selectedFlatRows);
-        setSelectedCommunities(
-          e.detail.selectedFlatRows.map((row) => row.original)
-        );
-      }}
-      {...otherprops}
-    />
+    <div slot={slot}>
+      <FilterBar
+        hideToolbar={true}
+        hideFilterConfiguration={true}
+        style={{ padding: "1rem" }}
+        showGoOnFB={true}
+        onGo={() => {}}
+      >
+        <FilterGroupItem label="City" required>
+          <MultiComboBox>
+            <MultiComboBoxItem text="Irvine" />
+          </MultiComboBox>
+        </FilterGroupItem>
+        <FilterGroupItem label="Num Bed">
+          <MultiComboBox>
+            <MultiComboBoxItem text="1" />
+          </MultiComboBox>
+        </FilterGroupItem>
+        <FilterGroupItem label="Num Bath">
+          <MultiComboBox>
+            <MultiComboBoxItem text="1" />
+          </MultiComboBox>
+        </FilterGroupItem>
+        <FilterGroupItem label="Max Price">
+          <Input />
+        </FilterGroupItem>
+        <FilterGroupItem label="Min Area">
+          <Input />
+        </FilterGroupItem>
+        <FilterGroupItem label="Amenities">
+          <Select></Select>
+        </FilterGroupItem>
+      </FilterBar>
+      <AnalyticalTable
+        header={
+          <Toolbar>
+            <ToolbarSpacer />
+            <Button onClick={handleShowDetails}>Show Details</Button>
+          </Toolbar>
+        }
+        data={communityList}
+        columns={columns}
+        groupBy={["city"]}
+        groupable={true}
+        filterable={true}
+        headerRowHeight={50}
+        selectionMode={AnalyticalTableSelectionMode.MultiSelect}
+        onRowSelect={(e) => {
+          setSelectedCommunities(
+            e.detail.selectedFlatRows.map((row) => row.original)
+          );
+        }}
+        NoDataComponent={() => (
+          <IllustratedMessage
+            subtitleText={"Select one or more cities to start"}
+          />
+        )}
+        {...otherprops}
+      />
+    </div>
   );
 }
